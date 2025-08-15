52b4071eef4e68f99d5af7f0616495bd91932cf3
Haitham-hub-debug <haithama788@gmail.com>
2025-08-15 20:57:42 +0200

    feat: Create ForgotPassword page - Added form to submit email for password reset - Display messages for success or errors - Linked page to /login for navigation

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -2,11 +2,15 @@ import { useState } from "react";
 import axios from "axios";
 import { useNavigate, Link } from "react-router-dom";
 import F_pass from "./F_pass";
+
+
+
 export default function Login() {
   const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
+const [showFPass, setShowFPass] = useState(false); // حالة لإظهار نموذج نسيت كلمة المرور
 
   const handleLogin = async (e) => {
     e.preventDefault();
@@ -24,26 +28,42 @@ export default function Login() {
   };
 
   return (
+     <div>
+      {!showFPass && (
+     <>
+
     <form onSubmit={handleLogin}>
-      <input
-        type="email"
-        placeholder="البريد الإلكتروني"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        required
-      />
-      <input
-        type="password"
-        placeholder="كلمة المرور"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        required
-      />
-      <button type="submit">تسجيل الدخول</button>
-      {error && <p style={{ color: "red" }}>{error}</p>}
-      <p>
-        لا تملك حساب؟ <Link to="/register">سجل هنا</Link>
-      </p>
-    </form>
+            <input
+              type="email"
+              placeholder="البريد الإلكتروني"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+            <input
+              type="password"
+              placeholder="كلمة المرور"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+            <br/>
+            <br/>
+            <button type="submit">تسجيل الدخول</button>
+          </form>
+           <br/>
+           <br/>
+          <button onClick={() => setShowFPass(true)}>
+            نسيت كلمة المرور؟
+          </button>
+
+          <p>
+            لا تملك حساب؟ <Link to="/register">سجل هنا</Link>
+          </p>
+        </>
+      )}
+
+      {showFPass && <F_pass />} {/* يظهر نموذج إعادة تعيين كلمة المرور */}
+  </div>
   );
 }
